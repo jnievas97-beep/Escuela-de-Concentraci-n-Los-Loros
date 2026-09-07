d6175b4edf8176dc2197b30ab1818d1a1cb2dfe6
jnievas97-beep <jnievas97@gmail.com>
2026-09-07 15:16:59 -0300

    Add files via upload

---
+++ notificaciones-config.js
@@ -0,0 +1,5 @@
+/* Configuración pública del receptor de suscripciones FCM.
+   Pega aquí la URL /exec del Web App de Apps Script al terminar el despliegue. */
+window.ESCUELA_NOTIFICACIONES = {
+  endpointRegistro: "https://script.google.com/macros/s/AKfycbxrR_Gbk40Kn2Q2Kmg7Si9CZs184bVJ9bzMqCugiS8McOCA21bp3GXR0aJR4JTNMYWIWg/exec"
+};
